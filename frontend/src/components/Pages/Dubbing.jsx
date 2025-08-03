43025f7d7199e6fdc162bc8f3e749772c7e065bc
RupakBoral <boralrupak@gmail.com>
2025-08-03 12:42:30 +0530

    dubbing ui

--- frontend/src/components/Pages/Dubbing.jsx
+++ frontend/src/components/Pages/Dubbing.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dubbing = () => {
-  return <div>Dubbing</div>;
+  const [error, setError] = useState(null);
+
+  const handleFile = async (event) => {
+    try {
+      if (event.target.files[0].type == "audio/*") {
+        event.target.value = "";
+        throw new Error("Please upload an audio file");
+      }
+      const audioFile = event?.target?.files[0];
+      const formData = new FormData();
+      formData.append("audio", audioFile);
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/upload-audio`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
+
+      const data = await response.json();
+      console.log("Uploaded audio URL:", data.audioUrl);
+    } catch (err) {
+      setError(err?.message);
+      setTimeout(() => {
+        setError("");
+      }, 3000);
+    }
+  };
+
+  return (
+    <div>
+      <h2 className="text-white">Dubbing</h2>
+      <input
+        accept="audio/wav, audio/mpeg"
+        className="text-white"
+        onChange={handleFile}
+        type="file"
+      />
+      {error ? (
+        <p className="text-red-500 font-semibold text-lg">{error}</p>
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
 };
 
 export default Dubbing;

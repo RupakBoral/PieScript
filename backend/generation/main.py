20c63a40699b4baa6f76c66f46368def2d3d3681
RupakBoral <boralrupak@gmail.com>
2025-08-05 12:30:46 +0530

    favicon changed

--- backend/generation/main.py
+++ backend/generation/main.py
@@ -2,14 +2,8 @@ from manim import *
 
 class Main(Scene):
  def construct(self):
-  axes = Axes(x_range=[0, 4 * PI, PI], y_range=[-1.5, 1.5, 1], color=BLUE)
+  axes = Axes(x_range=[-4*PI, 4*PI, PI], y_range=[-1.5, 1.5, 1], color=WHITE)
+  graph = axes.plot(lambda x: np.sin(x), x_range=[-4*PI, 4*PI], color=BLUE)
   self.play(Create(axes))
-
-  sine_wave = axes.plot(lambda x: np.sin(x), x_range=[0, 4 * PI], color=YELLOW)
-  self.play(Create(sine_wave))
-
-  dot = Dot().move_to(axes.coords_to_point(0, np.sin(0)))
-  self.play(FadeIn(dot))
-
-  self.play(dot.animate.move_to(axes.coords_to_point(2 * PI, np.sin(2 * PI))), run_time=2)
+  self.play(Create(graph))
   self.wait(0.5)
